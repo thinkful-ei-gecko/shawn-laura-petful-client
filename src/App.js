0052a959e26eef538d26f10b22a45cd4fa9a852f
laura <laura.j.elias@gmail.com>
2019-11-15 14:18:33 -0600

    establish basic components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
+import LandingPage from './components/LandingPage';
+import PetsPage from './components/PetsPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+
+  render(){
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path='/' component={LandingPage} />
+            <Route exact path='/pets' component={PetsPage} />
+          </Switch>
+        </Router>
+
+      </div>
+    );
+  
+  }
 }
 
 export default App;
---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '.LandingPage.css';
+
+class LandingPage extends Component{
+  render(){
+    return(<>
+
+    </>);
+  }
+}
+export default LandingPage;
