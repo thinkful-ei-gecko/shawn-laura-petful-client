078dc48a29ead0bbcfb7a3e67bc7684b5f1bebf5
laura <laura.j.elias@gmail.com>
2020-01-08 10:37:34 -0600

    url correction

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   REACT_APP_API_BASE:
     process.env.NODE_ENV === 'production'
-      ? 'https://shawn-laura-petful-server.herokuapp.com'
+      ? 'https://shawn-laura-petful-server.herokuapp.com/api'
       : 'http://localhost:8000/api'
 }
 
